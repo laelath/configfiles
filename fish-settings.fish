dbf23e357b3c6758a4c481bc42b8f5065c2a2021
laelath <jf.laelath@gmail.com>
2017-04-26 19:38:10 -0700

    Updated fish colors and prompt

--- fish-settings.fish
+++ fish-settings.fish
@@ -1,23 +1,23 @@
 #!/bin/fish
 set fish_color_autosuggestion brblack
-set fish_color_command blue
-set fish_color_comment red
-set fish_color_cwd green
-set fish_color_cwd_root red
-set fish_color_end green
+set fish_color_command brwhite
+set fish_color_comment brgray
+set fish_color_cwd brgreen
+set fish_color_cwd_root brred
+set fish_color_end brorange
 set fish_color_error brred
-set fish_color_escape cyan
-set fish_color_history_current cyan
+set fish_color_escape brorange
+set fish_color_history_current brcyan
 set fish_color_host normal
-set fish_color_match cyan
-set fish_color_normal white
-set fish_color_operator cyan
-set fish_color_param brcyan
-set fish_color_quote yellow
-set fish_color_redirection cyan
+set fish_color_match brcyan
+set fish_color_normal brwhite
+set fish_color_operator normal
+set fish_color_param brblue
+set fish_color_quote brgreen
+set fish_color_redirection brpurple
 set fish_color_search_match --background purple
 set fish_color_selection --background purple
-set fish_color_status red
-set fish_color_user green
+set fish_color_status brred
+set fish_color_user brgreen
 set fish_color_valid_path --underline
 set fish_greeting
